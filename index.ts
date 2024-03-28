47c7d9fdbb5832a06f51b11a2f4b902cc44d6dc2
sardarqayum <161648541+sardarqayum@users.noreply.github.com>
2024-03-28 06:18:40 +0500

    added console

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer";
+
 const randomNumber = Math.floor(Math.random() *6 + 1);
+
+console.log("Welcome to number Guessing Game");
 
 const answers = await inquirer.prompt([
     {
